5d9174fa1b333cc25251907345ef18ef3f13d982
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-04-02 18:29:49 -0400

    added footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>Connect with me:</p>
+      <div className="social-links">
+        <a href="#">GitHub</a>
+        <a href="#">LinkedIn</a>
+        <a href="#">Twitter</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
