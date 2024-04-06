5121902527bcb6dcc0f162c24c44896b5aa5c331
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-04-06 16:32:01 -0400

    completed about me page

---
+++ src/pages/AboutMe.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <div className="content">
+      <h2>About Me</h2>
+      <div className="bio">
+        <img src="../../public/images/headshot.jpg" alt="Christian Tino" />
+        <p>Hello! I'm Christian Tino, a recent coding boot camp graduate with a passion for software development and a background in dental software systems. I'm on an exciting journey to transition into the dynamic world of tech.
+
+My professional experience has equipped me with a diverse skill set ranging from CAD/CAM design to software support, honing my ability to adapt and thrive in fast-paced environments. As a CAD/CAM Nightguard Designer at Dandy, I leveraged advanced software tools like 3Shape Splint Studio and MeshMixer while spearheading the development of Standard Operating Procedures.
+
+My educational pursuit at the University of North Carolina Charlotte's Full-Stack Coding Bootcamp has further enriched my skills in HTML, CSS, JavaScript, and Node.js. From crafting responsive client-side applications to co-engineering full-stack web applications using Agile methodologies, I've been immersed in collaborative projects that have enhanced my problem-solving abilities.
+
+In my recent projects, such as "Dad vs Mom Jokes" and "Req, Res," I've demonstrated my proficiency in developing intuitive user interfaces and integrating third-party APIs to enhance functionality. Whether it's managing reservations or facilitating seamless software integrations, I thrive on tackling complex challenges and delivering innovative solutions.
+
+Driven by a desire to continuously grow and learn, I'm actively seeking intern opportunities or entry-level positions where I can apply my newly acquired software development skills in a collaborative and dynamic environment. With a strong foundation in both dental software systems and web development, I'm excited to contribute to the innovative solutions shaping the future of technology.
+
+Let's connect and explore how I can contribute to your team's success!</p>
+      </div>
+    </div>
+  );
+}
+
+export default AboutMe;
