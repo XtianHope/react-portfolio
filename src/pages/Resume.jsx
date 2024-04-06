297d98f3754f0507c8fd7cb1a6a89d71deb182d6
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-04-06 16:49:32 -0400

    added resume page

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Resume() {
+  return (
+    <div className="content">
+      <h2>Resume</h2>
+      <p>https://docs.google.com/document/d/15jS9YSkvBBlItAWp_xvUTPiEZ5mFWiAg_brgq9KMCQU/edit</p>
+      <p>Proficiencies</p>
+      <ul>
+        <li>UX/UI</li>
+        <li>Sofware Systems</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Resume;
