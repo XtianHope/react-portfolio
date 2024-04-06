906ba5ff5f1049f00ee6a04b30914603804f372d
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-04-06 16:54:32 -0400

    worked on index.html and updated proficiencies for resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -9,6 +9,40 @@ function Resume() {
       <ul>
         <li>UX/UI</li>
         <li>Sofware Systems</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Git</li>
+        <li>JavaScript</li>
+        <li>Bootstrap</li>
+        <li>Bulma</li>
+        <li>DOM Manipulation</li>
+        <li>API Integration</li>
+        <li>jQuery</li>
+        <li>JSON</li>
+        <li>AJAX</li>
+        <li>Agile Development</li>
+        <li>Node.js</li>
+        <li>Object-Oriented Programming</li>
+        <li>Express.js</li>
+        <li>MySQL</li>
+        <li>Bcrypt</li>
+        <li>MVC Paradigm</li>
+        <li>Handlebars</li>
+        <li>Sequelize</li>
+        <li>NoSQL</li>
+        <li>MERN Stack</li>
+        <li>MongoDB</li>
+        <li>Mongoose</li>
+        <li>Jira</li>
+        <li>CAD/CAM Design</li>
+        <li>SaaS Experience</li>
+        <li>Standard Operating Procedure (SOP) Development</li>
+        <li>Laboratory Operations</li>
+        <li>Order Operations</li>
+        <li>Technical Support</li>
+        <li>3D STL Manipulation</li>
+        <li>3D Printing Software (e.g., 3Shape, MeshMixer, Orthoanalyzer)</li>
+        <li>Software Utilization: EasyOrthoRx, EasyRx, EnvisionOne, Itero</li>
       </ul>
     </div>
   );
